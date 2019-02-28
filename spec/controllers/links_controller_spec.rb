c9e3f0133dba90bd6e3de8da9b0bac6d8064d961
Anthony <anttyaadiggg@yahoo.com>
2019-02-28 03:50:56 -0500

    work in progress controller specs

--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -1,11 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe LinksController, type: :controller do
-  let!(:link) { create(:link) }
+  let(:valid_attributes) do
+      {
+        link: {
+          original_url: Faker::Internet.url,
+          shortened_url: Faker::Internet.slug,
+          hash_key: Faker::Alphanumeric.alpha
+        }
+      }.to_json
+    end
 
+    describe "GET #show" do
+      it "returns a success response" do
+        link = Link.create! valid_attributes
+        get :show, params: {id: link.to_param}, session: valid_session
+        expect(response).to be_successful
+      end
+    end
 
-  describe "controller actions" do
-
-  end
+  # describe "GET #new" do
+  #   it "returns a success response" do
+  #
+  #   end
+  # end
+  #
 
 end

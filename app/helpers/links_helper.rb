420e49a5e6fe24c883588d844da59883390d858b
Anthony <anttyaadiggg@yahoo.com>
2019-02-28 03:12:53 -0500

    Bug Fix: Links and admin links template fix
    Admin links template, added js remote to expire links, also rendered
    links count. Links fixed redirection to include hash_key, created link
    helper method to display shortened_url as proper url.

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,2 +1,7 @@
 module LinksHelper
+  def short_link_display(shortened_url)
+    #Wont work for Prod will need to refactor
+    #but for time being lets go with this
+    "http://localhost:3000/#{shortened_url}"
+  end
 end

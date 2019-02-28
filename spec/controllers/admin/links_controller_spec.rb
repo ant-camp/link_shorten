e32ac9f3d22523ff6d85177626184f71267fb4a4
Anthony <anttyaadiggg@yahoo.com>
2019-02-28 18:45:02 -0500

    Created specs to test links controller actions
    Created specs to test admin link show action, along with links
    controller specs to test expire_link, show and create actions.

--- spec/controllers/admin/links_controller_spec.rb
+++ spec/controllers/admin/links_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
-# 
-# RSpec.describe Admin::LinksController, type: :controller do
 #
-# end
+RSpec.describe Admin::LinksController, type: :controller do
+
+  describe "Before action" do
+    it {should use_before_action(:set_link)}
+  end
+
+  describe 'GET #show' do
+    link = FactoryBot.create(:link, expired: true, original_url: 'http://google.com')
+      it "should show link" do
+        get :show, params: { id: link.id }
+        expect(response).to be_successful
+    end
+  end
+
+end
--- spec/controllers/links_controller_spec.rb
+++ spec/controllers/links_controller_spec.rb
@@ -1,29 +1,49 @@
 require 'rails_helper'
 
 RSpec.describe LinksController, type: :controller do
-  let(:valid_attributes) do
-      {
-        link: {
-          original_url: Faker::Internet.url,
-          shortened_url: Faker::Internet.slug,
-          hash_key: Faker::Alphanumeric.alpha
-        }
-      }.to_json
+  describe "Before action" do
+    it { should use_before_action(:set_link) }
+  end
+
+	describe 'POST #create' do
+    it "should create and shorten url and redirect" do
+      post :create, params: { link: { original_url: 'http://test.com'} }
+      expect(Link.last.original_url).to eq 'http://test.com'
+      expect(response.response_code).to eq 302
+      expect(response).to redirect_to("/links/#{Link.last.id}")
+    end
+	end
+
+  describe 'GET #forward_to_link' do
+    before do
+      allow_any_instance_of(Link).to receive(:click_counter).and_return(true)
+    end
+    context 'valid link' do
+      link = FactoryBot.create(:link, expired: false, original_url: 'http://google.com')
+      it "should redirect to the original url" do
+        get :forward_to_link, params: { hash_key: link.hash_key }
+        expect(response.response_code).to eq 301
+        expect(response).to redirect_to(link.original_url)
+      end
+
     end
 
-    describe "GET #show" do
-      it "returns a success response" do
-        link = Link.create! valid_attributes
-        get :show, params: {id: link.to_param}, session: valid_session
+    context 'expired link' do
+      link = FactoryBot.create(:link, expired: true, original_url: 'http://google.com')
+      it "should render 404 if the link is expired" do
+        get :forward_to_link, params: { hash_key: link.hash_key }
+        expect(response.response_code).to eq 404
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    link = FactoryBot.create(:link, expired: true, original_url: 'http://google.com')
+    context 'Should render link/show' do
+      it "should show link" do
+        get :show, params: { id: link.id }
         expect(response).to be_successful
       end
     end
-
-  # describe "GET #new" do
-  #   it "returns a success response" do
-  #
-  #   end
-  # end
-  #
-
+  end
 end
